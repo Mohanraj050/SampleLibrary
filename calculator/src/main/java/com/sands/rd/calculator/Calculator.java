1caf7a92bcf5c42bbe1ba26d58bc0a2a72c7e4a6
Mohanraj050 <GitHub@0781>
2018-04-27 11:54:16 +0530

    initial commit

---
+++ calculator/src/main/java/com/sands/rd/calculator/Calculator.java
@@ -0,0 +1,20 @@
+package com.sands.rd.calculator;
+
+public class Calculator {
+
+    public int addition(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public int subtraction(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public int multiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    public int division(int firstNumber, int secondNumber) {
+        return firstNumber / secondNumber;
+    }
+}
